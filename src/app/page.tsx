15cfafaea1f8eea04884adaebc31869b429c1037
rnogara <r.nogara.dev@gmail.com>
2025-08-21 17:53:21 -0300

    feat: add stats component with followers data and dynamic chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Background from "./components/Background";
 import HomeComponent from "./components/HomeComponent";
 import News from "./components/News";
+import Stats from "./components/Stats";
 
 export default function Home() {
   return (
     <main className="w-full font-merienda text-amber-200">
       <Background />
-      <HomeComponent />
-      <News />
+      <div className="relative">
+        <HomeComponent />
+        <News />
+        <Stats />
+      </div>
     </main>
   );
 }
